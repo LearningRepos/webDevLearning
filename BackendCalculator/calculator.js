65491220b2f3fd1c95f555455faf9d00181e3170
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-04-11 02:14:48 -0700

    Added a bmi calculator along w/ normal
    HTTP server with nodemon/express/bodyparser

--- BackendCalculator/calculator.js
+++ BackendCalculator/calculator.js
@@ -12,9 +12,20 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 } );
 
+app.get("/bmicalculator", function(req,res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req, res){
   var one = Number(req.body.num1);
   var two = Number(req.body.num2);
   var result = one + two;
   res.send("The result of this calculation is " + result);
+} );
+
+app.post("/bmicalculator", function(req,res){
+  var height = Number(req.body.height);
+  var weight = Number(req.body.weight);
+  var bmi =  703 * (weight/ (height * height));
+  res.send("Your calculated bmi is " + bmi);
 } );
