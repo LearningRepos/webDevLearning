0f54756cbbae92d2addbdb0e8d6c816742f86f0c
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-04-15 16:24:18 -0700

    To-Do List server created with ejs/express/nodejs

---
+++ ToDoListv1/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const https = require("https");
+const bp = require("body-parser");
+app.use(bp.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+var inp = "";
+var inpCollection = [];
+var workItems = [];
+app.set('view engine', 'ejs');
+
+app.listen(3000, function(){
+  console.log("server running on port 3000");
+});
+
+app.get("/", function(req,res){
+  var today = new Date();
+  var options = { weekday:"long", day:"numeric", month: "long" };
+  var day = today.toLocaleDateString("en-us", options);
+
+  res.render("index", {listTitle : day, newItems:inpCollection} );
+});
+
+app.get("/work", function(req,res){
+  res.render("index", {listTitle : "Work List", newItems: workItems} );
+});
+
+app.get("/about", function(req,res){
+  res.render("about" );
+});
+
+app.post("/", function(req,res){
+  inp = req.body.task;
+  if(req.body.list === "Work"){
+    workItems.push(inp);
+    res.redirect("/work");
+  }
+  else{
+    inpCollection.push(inp);
+    res.redirect("/");
+  }
+
+
+} );
+
+app.post("/work", function(req,res){
+  inp = req.body.task;
+  workItems.push(inp);
+  res.redirect("/work");
+});
