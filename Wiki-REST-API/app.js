a1aa8aaa0a9c08aaba5fe79d7ef410b8f6c3572c
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-04-26 17:28:51 -0700

    A REST API that can be manipulated by Postman

--- Wiki-REST-API/app.js
+++ Wiki-REST-API/app.js
@@ -5,7 +5,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/wikiDB', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect('mongodb://localhost:27017/wikiDB', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 
 const app = express();
@@ -23,47 +28,102 @@ app.listen(3000, function() {
 
 const wikiSchema = {
   title: String,
-  content:String
+  content: String
 };
 
 const Wiki = mongoose.model("Article", wikiSchema);
 
-app.route("/articles").get(function(req,res){
-  Wiki.find(function(err,found){
-    if(err){
-      res.send(err);
-    }
-    else{
-      res.send(found);
-    }
-  });
-})
+app.route("/articles").get(function(req, res) {
+    Wiki.find(function(err, found) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(found);
+      }
+    });
+  })
 
-app.route("route").get(function(req,res){})
+  .post(function(req, res) {
+    const newWiki = new Wiki({
+      title: req.body.title,
+      content: req.body.content
+    });
 
-.post(function(req,res){
-  const newWiki = new Wiki({
-    title : req.body.title,
-    content: req.body.content
+    newWiki.save(function(err) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send("Success")
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+    Wiki.deleteMany(function(err) {
+      if (!err) {
+        res.send("Success deleting")
+      } else {
+        res.send(err);
+      }
+    });
   });
 
-  newWiki.save(function(err){
-    if(err){
-      res.send(err)
-    }
-    else{
-      res.send("Success")
-    }
-  });
-})
+app.route("/articles/:articleTitle")
 
-.delete(function(req,res){
-  Wiki.deleteMany(function(err){
-    if(!err){
-      res.send("Success deleting")
-    }
-    else{
-      res.send(err);
-    }
+  .get(function(req, res) {
+    Wiki.findOne({
+      title: req.params.articleTitle
+    }, function(err, found) {
+      if (found) {
+        res.send(found)
+      } else {
+        res.send("No article found");
+      }
+    });
+  })
+
+  .put(function(req, res) {
+    Wiki.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      },
+      function(err, result) {
+        if (!err) {
+          res.send(req.body.title + " " + req.body.content);
+        }
+      });
+  })
+
+  .patch(function(req, res) {
+
+    Wiki.update({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      },
+      function(err) {
+        if (!err) {
+          res.send("Success Updating");
+        }
+        else{
+          res.send(err);
+        }
+      }
+
+    );
+  })
+
+  .delete(function(req,res){
+    Wiki.deleteOne({title: req.params.articleTitle}, function(err){
+      if(!err){
+        res.send("Success Deleting");
+      }
+      else{
+        res.send(err);
+      }
+    });
   });
-});
