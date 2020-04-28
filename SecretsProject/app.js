25ada92530e3ff4a75cddcf2d85c66223104af4b
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-04-27 23:15:23 -0700

    Secrets Project with hashing and salting
    no passport js or O auth here

--- SecretsProject/app.js
+++ SecretsProject/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -11,15 +12,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true
 });
 
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.listen("3000", function() {
-  console.log("Server started on port 3000")
+  console.log("Server started on port 3000");
 });
 
 app.get("/", function(req, res) {
@@ -52,9 +52,11 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (found) {
-        if (found.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, found.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
@@ -65,15 +67,20 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
+
 });
