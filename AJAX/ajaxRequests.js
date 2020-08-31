5ff08cb9172075951af5b9508de38171c012e079
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-08-30 17:29:09 -0700

    Finished xhr, fetch, jQuery, and axios ajax requests

--- AJAX/ajaxRequests.js
+++ AJAX/ajaxRequests.js
@@ -76,3 +76,71 @@ button.addEventListener("click", function () {
       document.querySelector("#city").innerHTML = data.results[0].location.city;
     });
 });
+
+// using bacon ipsum and a dog pic generator with jquery ajax calls(remember your cdn)
+$("#btn").click(function () {
+  $.ajax({
+    method: "GET",
+    url: "https://baconipsum.com/api/?type=meat-and-filler",
+  })
+    .done(function (result) {
+      let baconText = document.querySelector("#bacon");
+      baconText.innerHTML = result[0];
+    })
+    .fail(function () {
+      console.log("error");
+    });
+
+  $.get("https://dog.ceo/api/breeds/image/random")
+    .done(function (data) {
+      $("#dog").attr("src", data.message);
+      console.log(data.message);
+    })
+    .fail(function () {
+      console.log("ERROR!");
+    });
+});
+
+// xhr, fetch, jquery, and axios(need cdn) compared all at once
+var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+let button1 = document.querySelector("#xhr");
+button1.addEventListener("click", function () {
+  const XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState === 4) {
+      if (XHR.status === 200) {
+        document.querySelector("#quote").innerHTML = JSON.parse(
+          XHR.responseText
+        );
+      } else {
+        console.log("error");
+      }
+    }
+  };
+
+  XHR.open("GET", url);
+  XHR.send();
+});
+
+let button2 = document.querySelector("#fetch");
+button2.addEventListener("click", function () {
+  fetch(url)
+    .then((result) => result.json())
+    .then((data) => (document.querySelector("#quote").innerHTML = data));
+});
+
+let button3 = document.querySelector("#jquery");
+button3.addEventListener("click", function () {
+  $.get(url).done(function (data) {
+    $("#quote").html(data);
+  });
+});
+
+let button4 = document.querySelector("#axios");
+button4.addEventListener("click", function () {
+  axios.get(url).then(function (result) {
+    document.querySelector("#quote").innerHTML = result.data[0];
+  });
+});
