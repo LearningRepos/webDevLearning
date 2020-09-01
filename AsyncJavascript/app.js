c3197b94fa892606b18dca8b004b5666363da9f5
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-08-31 22:53:12 -0700

    Finished array methods

--- AsyncJavascript/app.js
+++ AsyncJavascript/app.js
@@ -8,6 +8,21 @@ function callbackAnimal(functionName) {
   functionName(animal);
 }
 
+let testArray = [1, 2, 3, 4, 5];
+let wordArray = ["colt", "matt", "tim", "udemy"];
+let nameArray = [
+  { name: "Elie" },
+  { name: "Tim" },
+  { name: "Matt" },
+  { name: "Colt" },
+];
+let fullArray = [
+  { first: "Elie", last: "Schoppik" },
+  { first: "Tim", last: "Garcia" },
+  { first: "Matt", last: "Lane" },
+  { first: "Colt", last: "Steele" },
+];
+
 // foreach method
 let array = [1, 2, 3, 4, 5];
 
@@ -21,6 +36,52 @@ function twoMethod(array, callback) {
   for (let i = 0; i < array.length; i++) {
     callback(array[i], i, array);
   }
+}
+
+function doubleValues(arr) {
+  let newArr = [];
+  arr.forEach(function (val) {
+    newArr.push(val * 2);
+  });
+  return newArr;
+}
+
+function onlyEvenValues(arr) {
+  let newArr = [];
+  arr.forEach(function (val) {
+    if (val % 2 === 0) {
+      newArr.push(val);
+    }
+  });
+  return newArr;
+}
+
+function showFirstAndLast(arr) {
+  let newArr = [];
+  arr.forEach(function (val) {
+    newArr.push(val[0] + val[val.length - 1]);
+  });
+  return newArr;
+}
+
+function addKeyAndValue(arr, keyName, value) {
+  let newArr = [];
+  arr.forEach(function (val, index) {
+    newArr.push(val);
+    newArr[index][keyName] = value;
+  });
+  return newArr;
+}
+
+function vowelCount(str) {
+  let newVowels = {};
+  for (i of str) {
+    if (["a", "e", "i", "o", "u"].includes(i.toLowerCase())) {
+      let LL = i.toLowerCase();
+      newVowels[LL] ? newVowels[LL]++ : (newVowels[LL] = 1);
+    }
+  }
+  return newVowels;
 }
 
 // find index method
@@ -85,3 +146,65 @@ firstPromise
   .catch(function (result) {
     console.log("Number less than 0.5");
   });
+
+//map creates a new array and loops over the entire array
+function doubleValuesMap(arr) {
+  return arr.map((value) => (value *= 2));
+}
+
+function valTimesIndex(arr) {
+  return arr.map((value, index) => (value *= index));
+}
+
+function extractKey(arr, key) {
+  return arr.map((value) => value.name);
+}
+
+function extractFullName(arr) {
+  return arr.map((value) => value.first + " " + value.last);
+}
+
+//filer returns a new array with elements that pass a specific test
+function find(arr, searchValue) {
+  let filteredArray = arr.filter((value) => value === searchValue);
+  if (filteredArray.length) {
+    return filteredArray[0];
+  } else {
+    return "undefined";
+  }
+}
+
+//some returns true if 1 test is passed and every returns true if all tests are oassed
+function hasOddNumber(arr) {
+  return arr.some(function (value) {
+    return value % 2 != 0;
+  });
+}
+
+function hasAllOddNumber(arr) {
+  return arr.every(function (value) {
+    return value % 2 != 0;
+  });
+}
+
+//reduce has an accumulator which can add all the elements of an array up
+function sumUp(arr) {
+  return arr.reduce(function (accumulator, value) {
+    console.log(accumulator, value);
+    return accumulator + value;
+  });
+}
+
+// doubleValues(testArray);
+// onlyEvenValues(testArray)
+// showFirstAndLast(wordArray)
+// addKeyAndValue(nameArray,"title","instructor")
+// vowelCount("Elie")
+// doubleValuesMap(testArray)
+// valTimesIndex(testArray)
+// extractKey(nameArray,"name")
+// extractFullName(fullArray);
+// find(testArray,6);
+// hasOddNumber(testArray)
+// hasAZero(12120121);
+// sumUp(testArray)
